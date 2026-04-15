9fc7ac030bcf1677b483abcf81a92ede17fc3942
2023A51012 <2303A51012@sru.edu.in>
2026-04-15 11:30:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# space-shooter
+# 🚀 Space Shooter Game (Unity)
+
+A simple space shooter game built using Unity and C#.
+
+## Controls
+- Move: WASD / Arrow Keys
+- Shoot: Spacebar
+- Restart: R
---
+++ SpaceShooterAllInOne.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class SpaceShooterAllInOne : MonoBehaviour
+{
+    public static SpaceShooterAllInOne Instance;
+
+    void Start()
+    {
+        Debug.Log("Space Shooter Game Started");
+    }
+}
